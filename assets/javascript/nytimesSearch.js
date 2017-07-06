f691046396c99ec71ae64f85899a74b96cf16599
S-Voss <stephen.voss24@gmail.com>
2017-07-05 20:19:09 -0400

    Added the basic ajax functionality.

--- assets/javascript/nytimesSearch.js
+++ assets/javascript/nytimesSearch.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
 	$("#search-article").on("click", function(){
 		var search = $(this).val();
 	});
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response){
+    //Show the response the ajax retrieves
+    console.log(response);
+
+    //Declare a variable that will store the response
+    var results = response.data;
+
+
+
+
+  })
+
 });
